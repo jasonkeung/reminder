238d809ea3b48bd6a9271766003d398b4b42e29b
Jason Keung <jason.keungg@gmail.com>
2025-04-01 21:01:43 -0400

    Try REACT_APP_API_URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 class ApiClient {
     async get(endpoint, options = {}) {
